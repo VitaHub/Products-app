84626581935bb8b27af2286b294a09252b7b07c9
vitahub <vhubenko@warfare-tec.com>
2018-01-15 20:57:50 +0200

    categories#index action created

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
+  has_one :sample_product, -> { limit(1) }, class_name: 'Product'
   validates :name, presence: true, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, :image, :score, :price, :url, :category, presence: true
+  default_scope { order(score: :desc) }
 end
