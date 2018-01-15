8f1ff1fc2a2ca1d05bcbbf95a9b82a0f5103552b
vitahub <vhubenko@warfare-tec.com>
2018-01-15 21:09:42 +0200

    Pagination for categories list

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.order(:name).includes(:sample_product)
+                          .paginate(page: params[:page],
+                                    per_page: Category::PER_PAGE)
   end
 
   def show; end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :products
   has_one :sample_product, -> { limit(1) }, class_name: 'Product'
   validates :name, presence: true, uniqueness: true
+  PER_PAGE = 20
 end
