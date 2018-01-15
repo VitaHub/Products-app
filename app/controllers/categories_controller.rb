46e60a71091484a4b44f6b23c379a55b2d688b98
vitahub <vhubenko@warfare-tec.com>
2018-01-15 21:20:56 +0200

    categories#show action updated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,8 @@ class CategoriesController < ApplicationController
                                     per_page: Category::PER_PAGE)
   end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products
+  end
 end
